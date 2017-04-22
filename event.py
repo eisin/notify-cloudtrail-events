a0c2eab7e81debd0fbb4e03b35adc502f722a414
EC2 Default User <ec2-user@ip-172-31-11-27.ap-northeast-1.compute.internal>
2017-04-22 17:09:17 +0000

    added whole modules

---
+++ event.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+from collections import OrderedDict
+import boto3
+
+class EventBase():
+    def make_header(self):
+        event_time = datetime.fromtimestamp(self.EventTime, self.tz)
+        #event_time_disp = event_time.strftime("%Y/%m/%d %H:%M")
+        event_time_disp = event_time.strftime("%Y/%m/%d(%a) %H:%M").decode('utf-8')
+        #return u"■{1} {0.EventName} ({0.Username})".format(self, event_time_disp)
+        return u"\n".join((
+            u"■{1} ({0.Username})".format(self, event_time_disp),
+            u"{0.EventName}".format(self),
+        ))
+
+class EventInstance(EventBase):
+    def __init__(self, event, tz):
+        self.tz = tz
+        for key in event.keys():
+            setattr(self, key, event.get(key))
+
+    def display(self):
+        header = self.make_header()
+        parameters = OrderedDict()
+        for resource in self.Resources:
+            if resource.get("ResourceType") == "AWS::EC2::Instance":
+                instance_name = EventInstance.get_instance_name(resource.get("ResourceName"))
+                if not instance_name is None:
+                    parameters[resource.get("ResourceType")] = "{0} ({1})".format(
+                        resource.get("ResourceName"), instance_name)
+                continue
+            parameters[resource.get("ResourceType")] = resource.get("ResourceName")
+                
+        output = [header]
+        for param in  parameters.keys():
+            output.append(u"    [{0}] {1}".format(param, parameters[param]))
+        return u"\n".join(output)
+
+    @classmethod
+    def get_instance_name(cls, target_instance_id):
+        instances_list = EventInstance.instances()
+        for i in instances_list:
+            if i.instance_id == target_instance_id:
+                for tag in i.tags:
+                    if tag['Key'] == 'Name':
+                        return tag['Value']
+        return None
+
+    @classmethod
+    def instances(cls):
+        if cls._instances is None:
+            cls._instances = cls.retreive_instances()
+        return cls._instances
+    #instances._instances = None
+    _instances = None
+
+    @classmethod
+    def retreive_instances(cls):
+        result = []
+        ec2 = boto3.resource('ec2')
+        for instance in ec2.instances.all():
+            result.append(instance)
+        return result
+                
+        
+
+class EventOther(EventBase):
+    #ignore_field = ['EventId', 'EventTime', 'tz', 'CloudTrailEvent', 'EventName', 'Username']
+    ignore_field = []
+    def __init__(self, event, tz):
+        self.tz = tz
+        for key in event.keys():
+            setattr(self, key, event.get(key))
+
+    def display(self):
+        header = self.make_header()
+        #return header
+        #event_dict = vars(self)
+        output = [header]
+        for resource_dict in self.Resources:
+            if resource_dict.get('ResourceType') in self.ignore_field:
+                continue
+            output.append(u"    [{0}] {1}".format(resource_dict.get('ResourceType'), resource_dict.get('ResourceName')))
+        return u"\n".join(output)
---
+++ service.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+#from event import *
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+
+import datetime
+import pytz
+import locale
+
+import event
+
+def handler(event, context):
+    tz = pytz.timezone('Asia/Tokyo')
+    locale.setlocale(locale.LC_ALL, 'ja_JP.utf-8')
+
+    f = open('lookup_events.txt')
+    json_text = f.read()
+    f.close()
+
+    logs = json.loads(json_text)
+    event_list = logs.get("Events")
+    event_list.reverse()
+    for event_dict in event_list:
+        event = event_new(event_dict, tz)
+        print event.display()
+    
+    return 0
+
+def event_new(event_dict, tz):
+    if event_dict.get("EventName") in ['StartInstances', 'StopInstances']:
+        return event.EventInstance(event_dict, tz)
+    return event.EventOther(event_dict, tz)
